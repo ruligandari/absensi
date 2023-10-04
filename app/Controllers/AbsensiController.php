2440d72de48b9e3018256b01a9cbea0fd9b4a8c2
ruligandari <ruligandari@gmail.com>
2023-10-04 20:49:10 +0700

    menambahkan status terlambat

--- app/Controllers/AbsensiController.php
+++ app/Controllers/AbsensiController.php
@@ -10,7 +10,6 @@ class AbsensiController extends BaseController
 {
     public function index()
     {
-        
     }
 
     public function create($id)
@@ -32,33 +31,47 @@ class AbsensiController extends BaseController
         foreach ($dataMahasiswa as $mahasiswa) {
             $jam_masuk = strtotime($mahasiswa['jam_masuk']);
             $jam_keluar = strtotime($mahasiswa['jam_keluar']);
-            if ($date == $mahasiswa['tanggal'] && $time >= $jam_masuk && $time <= $jam_keluar && $mahasiswa['status'] == 'tidak hadir') {
-            $status = 'hadir';
-            $namaMk = $mahasiswa['nama_mk'];
-            $id_waktu_presensi = $mahasiswa['id'];
 
-            } else if ($time >= $jam_keluar) {
-            $status = 'tidak hadir';
-            $namaMk = $mahasiswa['nama_mk'];
-            } else if ($status = 'hadir') {
-            $status = 'sudah';
-            $namaMk = $mahasiswa['nama_mk'];
+            // Hitung waktu batas telat (jam keluar + 15 menit)
+            $batas_telat = strtotime('+15 minutes', $jam_keluar);
+
+            if ($date == $mahasiswa['tanggal'] && $time >= $jam_masuk && $time <= $jam_keluar && $mahasiswa['status'] != 'hadir' && $mahasiswa['status'] != 'terlambat') {
+                $status = 'hadir';
+                $namaMk = $mahasiswa['nama_mk'];
+                $id_waktu_presensi = $mahasiswa['id'];
+            } else if ($date == $mahasiswa['tanggal']  && $time > $jam_keluar && $time <= $batas_telat && $mahasiswa['status'] != 'terlambat') {
+                $status = 'terlambat';
+                $namaMk = $mahasiswa['nama_mk'];
+                $id_waktu_presensi = $mahasiswa['id'];
+            } else if ($time > $batas_telat) {
+                $status = 'tidak hadir';
+                $namaMk = $mahasiswa['nama_mk'];
+            } else if ($mahasiswa['status'] == 'hadir') {
+                $status = 'sudah';
+                $namaMk = $mahasiswa['nama_mk'];
+            } else if ($mahasiswa['status'] == 'terlambat') {
+                $status = 'sudah';
+                $namaMk = $mahasiswa['nama_mk'];
             }
         }
+
 
         $data = [
             'status' => 'error',
             'msg' => 'Belum Ada Presensi ',
         ];
 
-        
-        if ($status == 'hadir'){
+
+        if ($status == 'hadir') {
             $presensiData->updateStatusByIdWaktuPresensi($id_waktu_presensi, $nim);
-            return json_encode(array('status' => 'success', 'msg' => 'Presensi '.$namaMk.  ' Berhasil'));
-        } else if ($status == 'tidak hadir'){
-            return json_encode(array('status' => 'error', 'msg' => 'Anda Sudah Melakukan Presensi '.$namaMk. ' Pada Tanggal '. $date. ' Jam '. date('H:i:s')));
-        }else if ($status == 'sudah'){
-            return json_encode(array('status' => 'error', 'msg' => 'Anda Sudah Melakukan Presensi '.$namaMk));
+            return json_encode(array('status' => 'success', 'msg' => 'Presensi ' . $namaMk .  ' hadir Berhasil'));
+        } else if ($status == 'terlambat') {
+            $presensiData->updateStatusByIdWaktuPresensiTerlambat($id_waktu_presensi, $nim);
+            return json_encode(array('status' => 'success', 'msg' => 'Presensi ' . $namaMk .  ' Terlambat, Berhasil'));
+        } else if ($status == 'tidak hadir') {
+            return json_encode(array('status' => 'error', 'msg' => 'Waktu Presinsi ' . $namaMk . ' Pada Tanggal ' . $date . ' Jam ' . date('H:i:s')));
+        } else if ($status == 'sudah') {
+            return json_encode(array('status' => 'error', 'msg' => 'Anda Sudah Melakukan Presensi ' . $namaMk));
         }
         return json_encode($data);
     }
--- app/Models/PresensiDataModel.php
+++ app/Models/PresensiDataModel.php
@@ -40,72 +40,79 @@ class PresensiDataModel extends Model
     protected $beforeDelete   = [];
     protected $afterDelete    = [];
 
-    public function getLastID(){
-    // get last id_supplier
-    $builder = $this->db->table('presensi');
-    $builder->select('id_presensi');
-    $builder->orderBy('id_presensi', 'DESC');
-    $builder->limit(1);
-    $query = $builder->get();
-    if ($query->getNumRows() > 0) {
-        $row = $query->getRow();
-        return $row->id_presensi;
-    } else{
-        return null;
+    public function getLastID()
+    {
+        // get last id_supplier
+        $builder = $this->db->table('presensi');
+        $builder->select('id_presensi');
+        $builder->orderBy('id_presensi', 'DESC');
+        $builder->limit(1);
+        $query = $builder->get();
+        if ($query->getNumRows() > 0) {
+            $row = $query->getRow();
+            return $row->id_presensi;
+        } else {
+            return null;
+        }
     }
-    }
-    public function joinTable(){
+    public function joinTable()
+    {
         $builder = $this->db->table('daftar_presensi');
-        $builder->join('mata_kuliah','daftar_presensi.id_mk = mata_kuliah.id_mk');
-        $builder->join('dosen','daftar_presensi.id_dosen = dosen.id_dosen');
-        $builder->join('kelas','daftar_presensi.id_kelas = kelas.id_kelas');
+        $builder->join('mata_kuliah', 'daftar_presensi.id_mk = mata_kuliah.id_mk');
+        $builder->join('dosen', 'daftar_presensi.id_dosen = dosen.id_dosen');
+        $builder->join('kelas', 'daftar_presensi.id_kelas = kelas.id_kelas');
         $query = $builder->get();
         return $query->getResultArray();
     }
-    public function getByDosen(){
+    public function getByDosen()
+    {
         $builder = $this->db->table('daftar_presensi');
-        $builder->join('mata_kuliah','daftar_presensi.id_mk = mata_kuliah.id_mk');
-        $builder->join('dosen','daftar_presensi.id_dosen = dosen.id_dosen');
-        $builder->join('kelas','daftar_presensi.id_kelas = kelas.id_kelas');
+        $builder->join('mata_kuliah', 'daftar_presensi.id_mk = mata_kuliah.id_mk');
+        $builder->join('dosen', 'daftar_presensi.id_dosen = dosen.id_dosen');
+        $builder->join('kelas', 'daftar_presensi.id_kelas = kelas.id_kelas');
         $builder->where('daftar_presensi.id_dosen', session()->get('id_dosen'));
         $query = $builder->get();
         return $query->getResultArray();
     }
-    public function cariData($id){
+    public function cariData($id)
+    {
         $builder = $this->db->table('daftar_presensi');
-        $builder->join('mata_kuliah','daftar_presensi.id_mk = mata_kuliah.id_mk');
-        $builder->join('dosen','daftar_presensi.id_dosen = dosen.id_dosen');
-        $builder->join('kelas','daftar_presensi.id_kelas = kelas.id_kelas');
+        $builder->join('mata_kuliah', 'daftar_presensi.id_mk = mata_kuliah.id_mk');
+        $builder->join('dosen', 'daftar_presensi.id_dosen = dosen.id_dosen');
+        $builder->join('kelas', 'daftar_presensi.id_kelas = kelas.id_kelas');
         $builder->where('id', $id);
         $query = $builder->get();
         return $query->getRowArray();
     }
-    public function byKelas($kelas){
+    public function byKelas($kelas)
+    {
         $builder = $this->db->table('presensi');
-        $builder->join('mata_kuliah','presensi.id_mk = mata_kuliah.id_mk');
-        $builder->join('mahasiswa','presensi.nim = mahasiswa.nim');
-        $builder->join('kelas','presensi.id_kelas = kelas.id_kelas');
-        $builder->join('daftar_presensi','presensi.id = daftar_presensi.id');
+        $builder->join('mata_kuliah', 'presensi.id_mk = mata_kuliah.id_mk');
+        $builder->join('mahasiswa', 'presensi.nim = mahasiswa.nim');
+        $builder->join('kelas', 'presensi.id_kelas = kelas.id_kelas');
+        $builder->join('daftar_presensi', 'presensi.id = daftar_presensi.id');
         $builder->where('daftar_presensi.id', $kelas);
         $query = $builder->get();
         return $query->getResultArray();
     }
-    public function byDosen(){
+    public function byDosen()
+    {
         $builder = $this->db->table('presensi');
-        $builder->join('mata_kuliah','presensi.id_mk = mata_kuliah.id_mk');
-        $builder->join('mahasiswa','presensi.nim = mahasiswa.nim');
-        $builder->join('kelas','presensi.id_kelas = kelas.id_kelas');
-        $builder->join('daftar_presensi','presensi.id = daftar_presensi.id');
+        $builder->join('mata_kuliah', 'presensi.id_mk = mata_kuliah.id_mk');
+        $builder->join('mahasiswa', 'presensi.nim = mahasiswa.nim');
+        $builder->join('kelas', 'presensi.id_kelas = kelas.id_kelas');
+        $builder->join('daftar_presensi', 'presensi.id = daftar_presensi.id');
         $builder->where('daftar_presensi.id_dosen', session()->get('id_dosen'));
         $query = $builder->get();
         return $query->getResultArray();
     }
-    public function byNama($nim){
+    public function byNama($nim)
+    {
         $builder = $this->db->table('presensi');
-        $builder->join('mata_kuliah','presensi.id_mk = mata_kuliah.id_mk');
-        $builder->join('mahasiswa','presensi.nim = mahasiswa.nim');
-        $builder->join('kelas','presensi.id_kelas = kelas.id_kelas');
-        $builder->join('daftar_presensi','presensi.id = daftar_presensi.id');
+        $builder->join('mata_kuliah', 'presensi.id_mk = mata_kuliah.id_mk');
+        $builder->join('mahasiswa', 'presensi.nim = mahasiswa.nim');
+        $builder->join('kelas', 'presensi.id_kelas = kelas.id_kelas');
+        $builder->join('daftar_presensi', 'presensi.id = daftar_presensi.id');
         $builder->where('mahasiswa.nim', $nim);
         $query = $builder->get();
         return $query->getResultArray();
@@ -121,5 +128,16 @@ class PresensiDataModel extends Model
         $builder->where('id', $id);
         $builder->where('nim', $nim);
         $builder->update(['status' => 'hadir']);
+    }
+    public function updateStatusByIdWaktuPresensiTerlambat($id, $nim)
+    {
+        $builder = $this->db->table('presensi');
+        // $builder->join('mata_kuliah','presensi.id_mk = mata_kuliah.id_mk');
+        // $builder->join('mahasiswa','presensi.nim = mahasiswa.nim');
+        // $builder->join('kelas','presensi.id_kelas = kelas.id_kelas');
+        // $builder->join('daftar_presensi','presensi.id = daftar_presensi.id');
+        $builder->where('id', $id);
+        $builder->where('nim', $nim);
+        $builder->update(['status' => 'terlambat']);
     }
 }
